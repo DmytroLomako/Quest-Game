d119e840b8e3e31163c95cfdb55ed697932f988f
Dima <you@example.com>
2024-06-21 18:59:39 +0600

    color

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
-question1 = input('вы гуляли по улице и пошел дождь спрячитесь под крышей или пойдете дальше (yes (спрятаться),no (пойти дальше))')
+from colorama import *
+question1 = input('вы гуляли по улице и пошел дождь спрячитесь под крышей или пойдете дальше ' + Fore.GREEN + '(yes (спрятаться), ' + Fore.BLUE + 'no (пойти дальше))')
 while question1 not in ['yes', 'no']:
-    question1 = input('вы гуляли по улице и пошел дождь спрячитесь под крышей или пойдете дальше (yes (спрятаться),no (пойти дальше))')
+    question1 = input(Fore.WHITE + 'вы гуляли по улице и пошел дождь спрячитесь под крышей или пойдете дальше ' + Fore.GREEN + '(yes (спрятаться), ' + Fore.BLUE + 'no (пойти дальше))')
 if question1 == 'yes':
-    print('вы не намокли и спрятались под крышей')
-    question2 = input('к вам подошел человек yes (поздороваться), no (проигрнорировать)')
+    print(Fore.GREEN + 'вы не намокли и спрятались под крышей')
+    question2 = input(Fore.WHITE + 'к вам подошел человек ' + Fore.GREEN + 'yes (поздороваться), ' + Fore.BLUE + 'no (проигрнорировать)')
     while question2 not in ['yes', 'no']:
-        question2 = input('к вам подошел человек yes (поздороваться), no (проигрнорировать)')
+        question2 = input(Fore.WHITE + 'к вам подошел человек ' + Fore.GREEN + 'yes (поздороваться), ' + Fore.BLUE + 'no (проигрнорировать)')
     if question2 == 'yes':
-        print('у вас появился новый друг и дождь закончился')
-        question4 = input('вы пошли в хорошем настроении домой с вашим новым другом и по дороге встретили реку хотите поплавать? yes, no')
+        print(Fore.GREEN + 'у вас появился новый друг и дождь закончился')
+        question4 = input(Fore.WHITE + 'вы пошли в хорошем настроении домой с вашим новым другом и по дороге встретили реку хотите поплавать? ' + Fore.RED + 'yes, ' + Fore.GREEN + 'no')
         while question4 not in ['yes', 'no']:
-            question4 = input('вы пошли в хорошем настроении домой с вашим новым другом и по дороге встретили реку хотите поплавать? yes, no')
+            question4 = input(Fore.WHITE + 'вы пошли в хорошем настроении домой с вашим новым другом и по дороге встретили реку хотите поплавать? ' + Fore.RED + 'yes, ' + Fore.GREEN + 'no')
         if question4 == 'yes':
-            print('вы неумеете плавать. вы утонули в реке')
-            print('game over. you can`t swim ending')
+            print(Fore.RED + 'вы неумеете плавать. вы утонули в реке')
+            print(Fore.RED + 'game over. you can`t swim ending')
         elif question4 == 'no':
-            print('вы с другом пришли домой и отлично провели день')
-            print('game over. best ending')
+            print(Fore.GREEN + 'вы с другом пришли домой и отлично провели день')
+            print(Fore.GREEN + 'game over. best ending')
     elif question2 == 'no':
-        print('вы продолжили стоять в одиночевстве под крышей')
-        question5 = ('дождь закончился. пойдете домой? yes, no')
+        print(Fore.BLUE + 'вы продолжили стоять в одиночевстве под крышей')
+        question5 = (Fore.WHITE + 'дождь закончился. пойдете домой?' + Fore.GREEN + ' yes,' + Fore.RED + ' no')
         while question5 not in ['yes', 'no']:
-            question5 = ('дождь закончился. пойдете домой? yes, no')
+            question5 = (Fore.WHITE + 'дождь закончился. пойдете домой?' + Fore.GREEN + ' yes,' + Fore.RED + ' no')
         if question5 == 'yes':
-            print('вы пришли домой в плохом настроении потому что стояли 3 часа под кышей в ожидании')
-            print('game over. normal ending')
+            print(Fore.GREEN + 'вы пришли домой в плохом настроении потому что стояли 3 часа под кышей в ожидании')
+            print(Fore.BLUE + 'game over. normal ending')
         elif question5 == 'no':
-            print('вас сбила машина')
-            print('why you stayed here ending')
+            print(Fore.RED + 'вас сбила машина')
+            print(Fore.RED + 'why you stayed here ending')
 elif question1 == 'no':
-    print('вы намокли и простыли')
+    print(Fore.BLUE + 'вы намокли и простыли')
     question3 = input('на вас идет подозрительный человек yes (поздороваться), no (проигрнорировать)')
     while question3 not in ['yes', 'no']:
         question3 = input('на вас идет подозрительный человек yes (поздороваться), no (проигрнорировать)')
